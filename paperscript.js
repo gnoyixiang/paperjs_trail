64111d220376634d038dec53a78983c7486fafb0
unknown <gnoyixiang@gmail.com>
2018-08-03 01:28:29 +0800

    completed all functionalities

--- paperscript.js
+++ paperscript.js
@@ -1,17 +1,28 @@
 paper.install(window);
 
+let items = [];
+let paths = [];
+
+let intersectionGroup = null;
+
+let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+let connections = ["AB", "AC", "BD", "BE", "CG", "DF", "EG", "FG"];
+
+let rectBounds = ["leftCenter", "rightCenter", "topCenter", "bottomCenter"];
+
+let selectedItem = null;
+let movedItem = false;
+let move_vector = null;
+
+let iterationCount = 0;
+let intervalID = null;
+let iterationStart = null;
+let iterationEnd = null;
+
+/*--------------------- start window events ---------------------*/
 
 window.onload = function () {
     paper.setup('myCanvas');
-
-    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-    let connections = ["AB", "AC", "BD", "BE", "CG", "DF", "EG", "FG"];
-    let items = [];
-    let paths = [];
-    let selectedItem = null;
-    let movedItem = false;
-    let move_vector = null;
-
     for (let i = 0; i < alphabet.length; ++i) {
         let x = 100 + 150 * i;
         let y = 100;
@@ -38,149 +49,257 @@ window.onload = function () {
 
         items.push(item);
 
-        item.onMouseDown = function(event){
-            console.log("item mousedown");
+        item.onMouseDown = function (event) {
             makeSelection(event.currentTarget);
-                      
+
             move_vector = selectedItem.position.subtract(event.point);
-        };            
+        };
+    }
+    makeConnections(items, connections);
+};
+
+window.onmouseup = function (event) {
+    if (selectedItem) {
+        selectionChange(items, selectedItem);
+    }
+    if (movedItem) {
+        removeMovedItem();
     }
 
-    makeConnections(connections);
+    selectedItem = null;
+}
 
-    function selectionChange(items, selectedItem) {
-        console.log("selection changed",selectedItem.firstChild.selected);
-        if (selectedItem.firstChild.selected) {  
-            console.log("moved item?",movedItem);          
-            if (!movedItem) {
-                
-                selectedItem.firstChild.selected = false;
-                unhighlightAll(items);
-            }
+window.onmousemove = function (event) {
+    movement_vector = new Point(event.movementX, event.movementY);
+    if (selectedItem && movement_vector.length > 1) {
+        selectedItem.position = new Point(event.x, event.y).add(move_vector);
+        makeConnections(items, connections);
+        highlightNeighbours(items, selectedItem);
+        movedItem = true;
+    }
+}
+
+window.ondblclick = function (event) {
+    // console.log("dblevent",event);
+    if (event.target.id != "myCanvas") return;
+    iterationStart = new Date();
+    document.getElementById("loader").style.visibility = "visible";
+    intervalID = window.setInterval(function () {
+        moveItems();
+        let now = new Date();
+        let timeElapsed = (now - iterationStart) / 1000
+        document.getElementById("timeElapsed").innerHTML = Math.round(timeElapsed) + "s";
+    }, 0);
+}
+
+/*--------------------- end window events ---------------------*/
+
+/*--------------------- functions list ---------------------*/
+
+function selectionChange(items, selectedItem) {
+    if (selectedItem.firstChild.selected) {
+        if (!movedItem) {
+
+            selectedItem.firstChild.selected = false;
+            unhighlightAll(items);
+        }
+    } else {
+        for (let i = 0; i < items.length; ++i) {
+            items[i].firstChild.selected = false;
+        }
+        selectedItem.firstChild.selected = true;
+        highlightNeighbours(items, selectedItem);
+    }
+
+}
+function unhighlightAll(items) {
+    for (let i = 0; i < items.length; ++i) {
+        items[i].firstChild.fillColor = "white";
+    }
+}
+
+function highlightNeighbours(items, selectedItem) {
+    nearestItem = null;
+
+    nearestElement = items.reduce(function (a, b) {
+        if (a == selectedItem) { return b; }
+        if (b == selectedItem) { return a; }
+
+        vector_a = a.position.subtract(selectedItem.position);
+        vector_b = b.position.subtract(selectedItem.position);
+
+        return vector_a.length < vector_b.length ? a : b;
+    });
+
+    nearestDistance = nearestElement.position.subtract(selectedItem.position).length;
+
+    for (let i = 0; i < items.length; ++i) {
+        if (items[i] == selectedItem) continue;
+        if (items[i].position.subtract(selectedItem.position).length < nearestDistance * 1.5) {
+            items[i].firstChild.fillColor = "yellow";
         } else {
-            for(let i=0;i<items.length;++i){
-                items[i].firstChild.selected = false;
-            } 
-            selectedItem.firstChild.selected = true;
-            highlightNeighbours(items, selectedItem);
-        }
-        
-    }
-    function unhighlightAll(items){
-        for(let i=0;i<items.length;++i){
-            items[i].firstChild.fillColor="white";
+            items[i].firstChild.fillColor = "white";
         }
     }
+}
 
-    function highlightNeighbours(items, selectedItem) {
-        console.log("highlight");
-        nearestItem = null;
+function makeSelection(item) {
+    selectedItem = item;
+    highlightNeighbours(items, selectedItem);
+}
 
-        nearestElement = items.reduce(function(a,b){            
-            if(a==selectedItem){ return b; }
-            if(b==selectedItem){ return a; }
-            
-            vector_a = a.position.subtract(selectedItem.position);
-            vector_b = b.position.subtract(selectedItem.position);
+function removeMovedItem() {
+    movedItem = false;
+    move_vector = null;
+}
 
-            return vector_a.length < vector_b.length ? a : b; 
-        });
-
-        // console.log("nearestElement", nearestElement);
-        nearestDistance = nearestElement.position.subtract(selectedItem.position).length;
-        // console.log("nearestDistance", nearestDistance);        
-
-        for(let i=0;i<items.length;++i){
-            if(items[i].position.subtract(selectedItem.position).length==nearestDistance){
-                console.log("item",items[i]);
-                items[i].firstChild.fillColor = "yellow";
-            } else{
-                items[i].firstChild.fillColor = "white";
-            }
+function makeConnections(items, connections, snap = true) {
+    if (paths) {
+        for (let i = 0; i < paths.length; ++i) {
+            paths[i].remove();
         }
+        paths = [];
     }
-
-    function connect(item1, item2) {
+    for (let i = 0; i < connections.length; ++i) {
+        firstAlphabetIndex = alphabet.indexOf(connections[i][0]);
+        secondAlphabetIndex = alphabet.indexOf(connections[i][1]);
+        connect(items[firstAlphabetIndex], items[secondAlphabetIndex], snap);
+    }
+}
+function connect(item1, item2, snap) {
+    if (snap) {
+        connection = getConnectionPoints(item1, item2);
+        point1 = connection.item1;
+        point2 = connection.item2;
+    } else {
         point1 = item1.bounds.rightCenter;
         point2 = item2.bounds.leftCenter;
-
-        var vector = point2.subtract(point1);
-        vector = vector.normalize(10);
-
-        vectorItem = new Group([
-            new Path([point1, point2]),
-            new Path([
-                point2,
-                point2.add(vector)
-            ]).rotate(160, point2),
-            new Path([
-                point2,
-                point2.add(vector)
-            ]).rotate(-160, point2)
-        ]);
-        vectorItem.strokeColor = "black";
-
-        paths.push(vectorItem);
     }
 
-    function makeConnections(connections) {
-        // console.log("paths", paths);
-        if (paths) {
-            for (let i = 0; i < paths.length; ++i) {
-                paths[i].remove();
+    var vector = point2.subtract(point1);
+    vector = vector.normalize(10);
+
+    vectorItem = new Group([
+        new Path([point1, point2]),
+        new Path([
+            point2,
+            point2.add(vector)
+        ]).rotate(160, point2),
+        new Path([
+            point2,
+            point2.add(vector)
+        ]).rotate(-160, point2)
+    ]);
+    vectorItem.strokeColor = "black";
+
+    paths.push(vectorItem);
+}
+function getConnectionPoints(item1, item2) {
+    let possibleConnections = [];
+    for (let i = 0; i < rectBounds.length; i++) {
+        for (let j = 0; j < rectBounds.length; j++) {
+            possibleConnections.push({
+                item1: item1.bounds[rectBounds[i]],
+                item2: item2.bounds[rectBounds[j]]
+            });
+        }
+    }
+    let connectionDistances = possibleConnections.map(function (conn) {
+        return conn.item1.subtract(conn.item2).length;
+    });
+    let minDistance = connectionDistances.reduce(function (a, b) {
+        return a < b ? a : b;
+    })
+    let connectionIndex = connectionDistances.indexOf(minDistance);
+    return possibleConnections[connectionIndex];
+}
+
+function clonePaths(pathsGroup, items, paths) {
+    for (let i = 0; i < items.length; ++i) {
+        pathsGroup.push(items[i].firstChild.clone());
+    }
+    for (let i = 0; i < paths.length; ++i) {
+        pathsGroup.push(paths[i].firstChild.clone());
+    }
+}
+
+function removeClonedPaths(pathsGroup) {
+    for (let i = 0; i < pathsGroup.length; ++i) {
+        pathsGroup[i].remove();
+    }
+}
+
+function hasIntersections(pathsGroup) {
+    intersectionGroup = [];
+    for (let i = 0; i < pathsGroup.length; ++i) {
+        for (let j = i + 1; j < pathsGroup.length; ++j) {
+            intersections = pathsGroup[i].getIntersections(pathsGroup[j]);
+            if (intersections.length > 0) {
+                for (let n = 0; n < intersections.length; n++) {
+                    if (verifyIntersection(intersections[n], pathsGroup[i], pathsGroup[j])) {
+                        return true;
+                    }
+                }
             }
-            paths = [];
-        }
-        for (let i = 0; i < connections.length; ++i) {
-            firstAlphabetIndex = alphabet.indexOf(connections[i][0]);
-            secondAlphabetIndex = alphabet.indexOf(connections[i][1]);
-            connect(items[firstAlphabetIndex], items[secondAlphabetIndex]);
         }
     }
+    return false;
+}
 
-    window.onmouseup = function(event){
-        console.log("window mouse up");
-        if (selectedItem) { 
-            selectionChange(items, selectedItem); 
+function verifyIntersection(intersection, pathsGroup1, pathsGroup2) {
+    let verify1 = true;
+    let verify2 = true;
+
+    function testIntersection(intersection, pathsGroup) {
+        return pathsGroup.segments.every(function (segment) {
+            return segment.point.subtract(intersection.point).length > 0
+        })
+    }
+
+    if (!pathsGroup1.closed) {
+        verify1 = testIntersection(intersection, pathsGroup1)
+    }
+    if (!pathsGroup2.closed) {
+        verify2 = testIntersection(intersection, pathsGroup2)
+    }
+    return verify1 && verify2;
+}
+
+function moveItems() {
+    let myCanvas = document.getElementById("myCanvas");
+    let myCanvasStartPoint = new Point(myCanvas.offsetLeft + 120, myCanvas.offsetTop + 120);
+    let myCanvasSize = new Point(myCanvas.offsetWidth - 200, myCanvas.offsetHeight - 200);
+
+    for (let i = 0; i < items.length; i++) {
+        items[i].position = myCanvasStartPoint.add(Point.random().multiply(myCanvasSize));
+    }
+    makeConnections(items, connections);
+    if (!hasIntersections(getPathsGroup(items, paths))) {
+        clearInterval(intervalID);
+        intervalID = null;        
+        let selectedIndex = getSelectedIndex(items);
+        if(selectedIndex!=-1){
+            highlightNeighbours(items,items[selectedIndex]);
         }
-        if (movedItem) {
-            //selectedItem.position = movedItem.position;
-            removeMovedItem();
-            //makeConnections(connections);
-        }
-        
-        selectedItem = null;
+        document.getElementById("loader").style.visibility = "hidden";
+        iterationCount = 0;
+        return;
     }
+    console.log("count", ++iterationCount);
+}
 
-    window.onmousemove = function(event){
-        console.log("window mouse moved");
-        // if (!movedItem && selectedItem) {
-        //     movedItem = selectedItem.clone();
-        // }
-        movement_vector = new Point(event.movementX,event.movementY);
-        // console.log(movement_vector.length);
-        if (selectedItem && movement_vector.length>1) {   
-            console.log("moved",movement_vector.length)         
-            selectedItem.position = new Point(event.x,event.y).add(move_vector);
-            makeConnections(connections);
-            highlightNeighbours(items,selectedItem);
-            movedItem = true;
-        }
+function getSelectedIndex(items){
+    for(let i=0;i<items.length;i++){
+        if(items[i].selected) return i;
     }
+    return -1;
+}
 
-
-    function makeSelection(item) {
-        selectedItem = item;
-        //selectionChange(items,selectedItem);
-        highlightNeighbours(items,selectedItem);
+function getPathsGroup(...pathItems) {
+    let pathsGroup = [];
+    for (let i = 0; i < pathItems.length; i++) {
+        for (let j = 0; j < pathItems[i].length; j++)
+            pathsGroup.push(pathItems[i][j].firstChild);
     }
-
-    function removeMovedItem() {
-        // movedItem.remove();
-        movedItem = false;
-        move_vector = null;
-    }
-
-    
-
-};
+    return pathsGroup;
+}
